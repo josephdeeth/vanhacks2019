e9d1e3eb754152bc0ca6b9643e097329bb5d5943
TedHaley <teddyhaley1@gmail.com>
2019-09-14 10:41:19 -0700

    add requirements and images

---
+++ image_recognition/image_recognition.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('messi5.jpg',0)
+img2 = img.copy()
+template = cv2.imread('template.jpg',0)
+w, h = template.shape[::-1]
+
