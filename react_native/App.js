66a2faf506162a16c8a011dddbca47a8d9ffd11c
Gordon Ng <gordon@berlinguettegroup.com>
2019-09-15 12:40:48 -0700

    working invisible search bar

--- react_native/App.js
+++ react_native/App.js
@@ -6,9 +6,8 @@ import { Platform, StatusBar, StyleSheet, View, Text, TextInput, TouchableOpacit
 import {Header, Button} from 'react-native-elements';
 import {Container, Content} from 'native-base';
 import Swiper from 'react-native-swiper';
-import SearchBar from 'react-native-search-bar'
 
-import * as gmaps from "https://maps.googleapis.com/maps/api/js?key=AIzaSyBihH7PrH5pN8qNefSev93BReEPg0uPD7c&libraries=places&callback=initMap";
+
 
 import * as WebBrowser from 'expo-web-browser';
 
@@ -48,62 +47,6 @@ handlePhoto = async () => {
   }
 }
 
-<!-- PARDON ME EXCUSE ME COMING THROUGH -->
-
-var map;
-var coordinates = {
-      lat: 49.2786042,
-      lng: -123.0998905,
-    };
-var infowindow;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: coordinates,
-    zoom: 12,
-  });
-
-  var request = {
-    query: 'Recycling Depot',
-    location: coordinates,
-    radius: 7500,  // meters
-  };
-
-  infowindow = new google.maps.InfoWindow();
-
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, function(results, status, pagination){
-    // alert(status);
-    // If we got results
-    if(status === google.maps.places.PlacesServiceStatus.OK){
-      for(var i = 0; i < results.length; i++){
-        // Mark them
-        createMarker(results[i]);
-      }
-
-      // And look at the top result
-      map.setCenter(results[0].geometry.location);
-    }
-  })
-}
-
-function createMarker(place){
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location,
-  });
-
-  // alert(place.name + " from the marker!");
-
-  google.maps.event.addListener(marker, "click", function(){
-    map.setCenter(marker.getPosition());
-    infowindow.setContent(place.name);
-    infowindow.open(map, marker);
-  });
-}
-
-<!-- PARDON ME EXCUSE ME I'M SORRY -->
 
 export default class App extends React.Component {
   state = { term: ''};
@@ -157,17 +100,10 @@ export default class App extends React.Component {
                 </View>
               </Camera>
             </View>
+        <View>
+        </View>
 
 
-            <View style={{ // height: 100,
-              margin: 0, padding: 0,}}>
-              <script src="map.js"></script>
-              <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&libraries=places&callback=initMap" async defer></script>
-
-              <Text>Map</Text>
-
-
-            </View>
           </Swiper>
         </Content>
       </Container>
