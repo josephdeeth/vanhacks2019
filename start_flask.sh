c5ee1cc64b02913ff26d85410672c063e1405ab4
TedHaley <teddyhaley1@gmail.com>
2019-09-14 20:45:45 -0700

    interchangeable bash command

--- start_flask.sh
+++ start_flask.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
 export FLASK_APP=rest_api
-flask run --host=192.168.43.200 --port=5000
+
+IP=$(ipconfig getifaddr en1)
+
+if [ "$1" = "public" ]
+then
+    echo Running publically
+    flask run --host="$IP" --port=5000
+else
+    echo Running locally
+    flask run
+fi
