fbadbfcff8ea39485fcccb7dff34b2aed9da8d21
Joey Deeth <joeydeeth@gmail.com>
2019-09-14 17:07:36 -0700

    added swipe navigation and camera functionality

--- react_native/App.js
+++ react_native/App.js
@@ -1,62 +1,97 @@
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, Component } from 'react';
+import { Platform, StatusBar, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import {Container, Content} from 'native-base';
+import Swiper from 'react-native-swiper';
 
-import AppNavigator from './navigation/AppNavigator';
+import * as WebBrowser from 'expo-web-browser';
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
+
+
+
+cameraRef = React.createRef();
+
+handlePhoto = async () => {
+  if(cameraRef){
+    let photo = await cameraRef.current.takePictureAsync();
+    console.log(photo);
   }
 }
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
+export default class App extends React.Component {
+  render() {
+    return(
+      <Container>
+        <Content>
+          <Swiper
+          loop={false}
+          index={1}
+          >
+            <View style={styles.slideDefault}>
+              <Text>Search</Text>
+            </View>
 
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+
+            <View style={{ flex: 1 }}>
+              <Camera ref={cameraRef} style={{ flex: 1 }}>
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: 'transparent',
+                    flexDirection: 'row',
+                  }}>
+                  <TouchableOpacity
+                    style={{
+                      flex: 0.1,
+                      alignSelf: 'flex-end',
+                      alignItems: 'center',
+                      bottom: 5,
+                    }}
+                    onPress={() => {
+                      this.setState({
+                        type:
+                          this.state.type === Camera.Constants.Type.back
+                            ? Camera.Constants.Type.front
+                            : Camera.Constants.Type.back,
+                      });
+                    }}>
+                  </TouchableOpacity>
+                </View>
+                <View style={{ alignItems: 'center' }}>
+                    <TouchableOpacity
+                      style={{width:60, height:60, borderRadius:30, backgroundColor:"#fff"}}
+                      onPress={this.handlePhoto} />
+                </View>
+              </Camera>
+            </View>
+
+
+            <View style={styles.slideDefault}>
+              <Text>Map</Text>
+            </View>
+          </Swiper>
+        </Content>
+      </Container>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  slideDefault:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#9DD6EB'
   },
-});
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
