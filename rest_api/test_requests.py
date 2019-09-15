62ea3d90e6ae5b3664da101aac3fdadb2b06655a
TedHaley <teddyhaley1@gmail.com>
2019-09-14 20:16:12 -0700

    Working with TEDNET

--- rest_api/test_requests.py
+++ rest_api/test_requests.py
@@ -4,9 +4,9 @@ import os
 
 def post_image(img_file):
     """ post image and return the response """
-    response = requests.post(url='http://127.0.0.1:5000/post_image',
+    response = requests.post(url='http://192.168.43.200:5000/post_image',
                              files={'file': open(img_file, 'rb')})
     print(response.content)
 
 
-post_image(img_file="/Users/teddyhaley/PycharmProjects/vanhacks2019/test_data/test_images/2˚.jpg")
+post_image(img_file="/Users/teddyhaley/PycharmProjects/vanhacks2019/test_data/test_images/2.jpg")
