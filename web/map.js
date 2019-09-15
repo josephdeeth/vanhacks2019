b9a477d6fa9195d0c5d186af8a90d6764de162ae
Ray Zhang <ray.zhang@alumni.ubc.ca>
2019-09-14 17:59:46 -0700

    Partial commit: attempting to debug map

--- web/map.js
+++ web/map.js
@@ -1,7 +1,42 @@
 var map;
+var coordinates = new google.maps.LatLng(49.2786042, -123.0998905);
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.2786042, lng: -123.0998905},
-    zoom: 4
+    center: coordinates,
+    zoom: 5,
+  });
+
+  var request = {
+    query: 'Recycling Depot',
+    location: coordinates,
+    radius: 10000,  // meters
+  };
+
+  var infowindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, function(results, status, pagination){
+    // If we got results
+    if(status === google.maps.places.PlacesServiceStatus.OK){
+      for(var i = 0; i < results.length; i++){
+        // Mark them
+        createMarker(results[i]);
+      }
+
+      // And look at the top result
+      map.setCenter(results[0].geometry.location);
+    }
+  })
+}
+
+function createMarker(place){
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addDomListener(marker, "click", function(){
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   });
 }
